4026a9ca2f95fe98ca828afd85c16ad934a930d6
GaryJz <mo0feng@qq.com>
2016-06-08 16:16:18 +0800

    Create datatables.go

---
+++ datatables.go
@@ -0,0 +1,148 @@
+package datatables
+
+import (
+	"github.com/astaxie/beego/orm"
+	"net/url"
+	"strconv"
+	"github.com/astaxie/beego"
+	"fmt"
+	"strings"
+	"unicode"
+)
+
+type DataTables interface {
+
+	Table(ctx url.Values, tb_name string,columns[] string, search_filter []string) (interface{},error)
+
+}
+
+var RegisterColumns map[string]interface{} = map[string]interface{}{
+
+}
+
+type Data struct {
+	Ctx				url.Values //get args
+	TableName   	string	//table name
+	Columns			[]string //select column
+	SearchFilter	[]string //where filter
+	//Model            interface{}
+}
+
+
+
+func (p *Data)Table() (rs interface{}, err error){
+	start,_ := strconv.Atoi(p.Ctx.Get("start"))
+	length,_ := strconv.Atoi(p.Ctx.Get("length"))
+	search := p.Ctx.Get("search[value]")
+	order_column,_ := strconv.Atoi(p.Ctx.Get("order[0][column]"))
+	order_dir := p.Ctx.Get("order[0][dir]")
+	draws,_ := strconv.Atoi(p.Ctx.Get("draw"))
+
+
+	//反射
+	//tb := reflect.TypeOf(p.Model)
+	//model_tb_name := tb.Elem().Name()
+	//beego.Info(model_tb_name)
+	//if len(model_tb_name) > 0{
+	//
+	//	return "1111111",nil
+	//}
+
+
+	//query field
+	var selectStr string
+	for k,v := range p.Columns{
+		if k != 0{ selectStr += ","}
+		selectStr += v
+	}
+	//beego.Info(selectStr)
+
+
+	//search
+	var whereStr  string
+	search_len := len(search)
+
+	//offset
+	colOffset := start
+	qb, _ := orm.NewQueryBuilder("mysql")
+	Str1 := "OperationRecord"
+
+	beego.Info(hump2Snakelike(Str1))
+
+
+	//search
+	if search_len >0 {
+		for k,v := range p.SearchFilter{
+			if k != 0 {
+				whereStr += " OR "}
+			whereStr +=  v + " LIKE " + "\"%"+search+"%\"" //like
+		}
+		if order_dir == "asc"{
+			qb.Select(selectStr).From(p.TableName).Where(whereStr).OrderBy(p.Columns[order_column]).Asc().Limit(length).Offset(colOffset)
+		}else{
+			qb.Select(selectStr).From(p.TableName).Where(whereStr).OrderBy(p.Columns[order_column]).Desc().Limit(length).Offset(colOffset)
+		}
+	}else{
+		if order_dir == "asc"{
+			qb.Select(selectStr).From(p.TableName).OrderBy(p.Columns[order_column]).Asc().Limit(length).Offset(colOffset)
+		}else{
+			qb.Select(selectStr).From(p.TableName).OrderBy(p.Columns[order_column]).Desc().Limit(length).Offset(colOffset)
+		}
+	}
+	sql := qb.String()
+	o := orm.NewOrm()
+
+	cl := RegisterColumns[p.TableName]
+	num, err := o.Raw(sql).QueryRows(cl)
+
+
+
+	recordTotal, err := o.QueryTable(p.TableName).Count() //data sum
+	var recordsFiltered int32 //search data sum
+	type rFilterCont struct {
+		CntNumber 		int
+	}
+	var rcount rFilterCont
+	if search_len >0 {
+		qb2, _ := orm.NewQueryBuilder("mysql")
+		qb2.Select("Count(*) AS cnt_number ").From(p.TableName).Where(whereStr)
+		sqlFilter := qb2.String()
+		o.Raw(sqlFilter).QueryRow(&rcount)
+		recordsFiltered = int32(rcount.CntNumber)
+	}else{
+		recordsFiltered = int32(recordTotal)
+	}
+
+	return map[string]interface{}{
+		"draw": int32(draws),
+		"recordsTotal": recordTotal,
+		"recordsFiltered": recordsFiltered,
+		"data":  func() (interface{}) {
+		if num == 0{
+			rest := []string {}
+			return rest
+		}else{
+			return cl
+		}}()},
+	err
+
+}
+
+
+func hump2Snakelike(hump string) string{
+
+	var snk string
+	for k,v := range hump{
+		if k == 0{
+			snk += strings.ToLower(fmt.Sprintf("%c",v))
+		}else{
+			if unicode.IsUpper(v){
+				snk += strings.ToLower(fmt.Sprintf("_%c",v))
+			}else{
+				snk += fmt.Sprintf("%c",v)
+			}
+		}
+	}
+
+	return  snk
+}
